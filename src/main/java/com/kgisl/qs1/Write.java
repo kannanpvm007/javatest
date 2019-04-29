960c115209e01352bd64d08cf8029b19004eec53
Kannan Ramakrishnan <kannan.r@kgisl.com>
2019-04-29 15:11:54 +0530

    First Commit

--- src/main/java/com/kgisl/qs1/Write.java
+++ src/main/java/com/kgisl/qs1/Write.java
@@ -1,17 +1,39 @@
 
 package com.kgisl.qs1;
 
-
+import java.io.File;
 import java.io.FileWriter;
+import java.util.Scanner;
+
+//import org.apache.poi.hssf.record.chart.ChartTitleFormatRecord;
 public class Write {
     public static void main(String[] args) {
-        try(FileWriter fileWriter = new FileWriter("D:/Kannan/test1/src/main/java/com/kgisl/qs1/hi.txt")) {
-            
-            //inherited method from java.io.Writer
-            fileWriter.write("my page writing in the operation");
-            fileWriter.write("welcome");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+       try
+       (FileWriter hi = new FileWriter("D:/Kannan/test1/src/main/java/com/kgisl/qs1/hi.txt")){
+         
+        Scanner se=   new Scanner(System.in);
+        System.out.println("enter your commants:");
+        String see = se.nextLine();
+       
+    //    for(int i =0; i<see.length();i++){
+        hi.write(see);
+    //    }
+        System.out.println("stored");
+        
+       System.out.println(" viwe text");
+       se.close();
+
+        File rv = new File("D:/Kannan/test1/src/main/java/com/kgisl/qs1/hi.txt");
+        Scanner  rev = new Scanner(rv);
+           // Scanner rev2 = rev;
+         //  rev.useDelimiter("\\Z");
+            System.out.println(rev.nextLine());
+            rev.close();
+
+       }
+       catch (Exception e){
+           System.out.println(e);
+
+       }
+  }
 }
